56b372f191c8a10ec53998b8f8ad1b55d4475825
Eugene Chou <euchou@ucsc.edu>
2025-02-26 04:32:56 +0000

    remove extra underscore from `pthread_rwlock_timedrdlock`

--- src/unix/linux_like/linux/mod.rs
+++ src/unix/linux_like/linux/mod.rs
@@ -6465,7 +6465,7 @@ extern "C" {
         policy: *mut c_int,
         param: *mut crate::sched_param,
     ) -> c_int;
-    pub fn pthread_rwlock_timedrd_lock(
+    pub fn pthread_rwlock_timedrdlock(
         rwlock: *mut crate::pthread_rwlock_t,
         abstime: *const crate::timespec,
     ) -> c_int;
